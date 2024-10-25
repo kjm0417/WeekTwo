eeba9478f00325b76ff003acf5581db07aa1a679
kjm0417 <ekdh41779@gmail.com>
2024-10-25 12:26:27 +0900

    오브젝트풀

--- Assets/ObjectPool.cs
+++ Assets/ObjectPool.cs
@@ -7,39 +7,84 @@ public class ObjectPool : MonoBehaviour
     [System.Serializable]
     public class Pool
     {
-        string name;
-        public GameObject Prefab;
+        public string name;
+        public GameObject prefab;
     }
 
     public int poolSize = 300;
 
     public List<Pool> pools = new List<Pool>();
 
+    private Dictionary<string , List<GameObject>> poolDictionary;
+
+    private GameObject currentObject;
+
     void Start()
     {
-        CratePool();
-    }
+        poolDictionary = new Dictionary<string, List<GameObject>>();
 
-    public void CratePool() //풀 생성
-    {
-        foreach(var pool in pools)
+        foreach (Pool pool in pools)
         {
-            for(int i = 0;i<poolSize; i++)
+            List<GameObject> objectPool = new List<GameObject>();
+
+            for (int i = 0; i <poolSize; i++)
             {
-                GameObject obj = Instantiate(pool.Prefab,transform);
+                GameObject obj = Instantiate(pool.prefab);
                 obj.SetActive(false);
+                objectPool.Add(obj);
             }
+
+            poolDictionary.Add(pool.name, objectPool);
         }
     }
 
-    //public GameObject Get()
-    //{
-    //    // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
-    //    if()
-    //}
 
-    public void Release(GameObject obj)
+    public GameObject Get(string name)
+    {
+        // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
+        if (poolDictionary.ContainsKey(name))
+        {
+            List<GameObject> pool = poolDictionary[name];
+            foreach(GameObject obj in pool)
+            {
+                if (!obj.activeInHierarchy)
+                {
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void Release(string name, GameObject obj)
     {
         // 게임오브젝트를 deactive한다.
+        if(poolDictionary.ContainsKey(name))
+        {
+            obj.SetActive(false );
+        }
+    }
+
+    public void ButtonOn(string name)
+    {
+        currentObject = Get(name);
+    }
+
+    public void ButtonOut(string name)
+    {
+
+        if (poolDictionary.ContainsKey(name))
+        {
+            List<GameObject> pool = poolDictionary[name];
+            foreach(GameObject obj in pool)
+            {
+                if(obj.activeInHierarchy)
+                {
+                    obj.SetActive(false);
+                    break;
+                }
+            }
+        }
     }
 }
